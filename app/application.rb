b314b76c90d7c13816619f715436d1547e64dd54
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-16 06:30:26 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    
+
+    if DateTime.now.strftime("%H:%M") >("12:00")
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+
+    resp.finish
+  end
+
+end
